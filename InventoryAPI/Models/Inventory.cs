eff75fe178757d8ca8c11cd7466c56a515fc3612
Yan L. Amorelli <yan.amorelli@trescon.com.br>
2021-11-28 20:46:16 -0300

    first commit

---
+++ InventoryAPI/Models/Inventory.cs
@@ -0,0 +1,19 @@
+﻿namespace InventoryAPI.Models
+{
+    public class Inventory
+    {
+        public int Id { get; set; }
+        public string InventoryName { get; set; }
+        public string InventoryDescription { get; set; }
+        public virtual List<Item> Items { get; set; }
+
+        public Inventory() { }
+        
+        public Inventory(int id, string inventoryName, string inventoryDescription)
+        {
+            Id = id;
+            InventoryName = inventoryName;
+            InventoryDescription = inventoryDescription;
+        }
+    }
+}
---
+++ InventoryAPI/Models/Item.cs
@@ -0,0 +1,28 @@
+﻿namespace InventoryAPI.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public int BuyPrice { get; set; }
+        public int SellPrice { get; set; }
+        public int InventoryId { get; set; }
+        public virtual Inventory Inventory { get; set; }
+
+
+        public Item() { }
+
+        public Item(int id, string name, string description, int quantity, int buyPrice, int sellPrice, int inventoryId)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Quantity = quantity;
+            BuyPrice = buyPrice;
+            SellPrice = sellPrice;
+            InventoryId = inventoryId;
+        }
+    }
+}
